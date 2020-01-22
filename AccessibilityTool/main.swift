6adf472540b3f322d78e14cef6658106c80a895b
savoptik <sav70792@gmail.com>
2020-01-22 12:55:23 +0400

    Initial Commit

---
+++ AccessibilityTool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AccessibilityTool
+//
+//  Created by Артём Семёнов on 22.01.2020.
+//  Copyright © 2020 Артём Семёнов. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
