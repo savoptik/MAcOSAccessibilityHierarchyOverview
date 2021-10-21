cf469873ce4fce32447fb8abb0ffd035630c21e5
Artem Semenov <Artem.Semenov@jetbrains.com>
2021-10-21 14:37:07 +0300

    update dependensy

--- Package.swift
+++ Package.swift
@@ -8,13 +8,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "MacOSAccessibilityApplicationWrapper", url: "https://github.com/savoptik/MacOSAccessibilityApplicationWrapper.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .executableTarget(
             name: "MAcOSAccessibilityHierarchyOverview",
-            dependencies: []),
+            dependencies: ["MacOSAccessibilityApplicationWrapper"]),
         .testTarget(
             name: "MAcOSAccessibilityHierarchyOverviewTests",
             dependencies: ["MAcOSAccessibilityHierarchyOverview"]),
