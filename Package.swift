23ef2423301296201accd1115ded1ef33f50313f
Artem Semenov <Artem.Semenov@jetbrains.com>
2021-10-25 11:46:36 +0300

    update swift package dependensy MAAW to https://github.com/savoptik/MacOSAccessibilityApplicationWrapper/releases/tag/v1.0

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "MacOSAccessibilityApplicationWrapper", url: "https://github.com/savoptik/MacOSAccessibilityApplicationWrapper.git", .branch("master"))
+        .package(name: "MacOSAccessibilityApplicationWrapper", url: "https://github.com/savoptik/MacOSAccessibilityApplicationWrapper.git", .branch("v1.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
